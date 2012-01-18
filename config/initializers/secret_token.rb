f4d059a280630036b43775065a5a88b096d1a368
pikulev91 <egor.pikulev@gmail.com>
2012-01-18 13:41:06 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytuby::Application.config.secret_token = '388f2ceeac390f070203f73ee3a14db4cef8c40be738329e5abd78de784589a4d2f7d06b320bb8cb710a8228b87390fd2fddf25cbf47956ccbe31562633ad5e8'
